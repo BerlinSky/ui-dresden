adc3f8ad642befaa62850963ecff9bdcd9018234
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-14 17:58:05 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,18 @@
+## UI - Layout
+
+### Steps:
+1. Set up scss files
+2. Set up fonts
+3. set up image files
+4. Set up pug files
+6. Configure the font-awesome-webpack2 loader 
+7. Download "font-awesome" zip
+8. Unzip the "font-awesome" zip to the sass folder
+9. Update main.scss to import font-awesome sass 
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,211 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'main.[chunkhash].css'
+});
+
+const providerPlugin = new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+})
+
+const cleanWebPackPlugin = new CleanWebpackPlugin(['dist'])
+
+const babelOptions = {
+  presets:  [
+    [ 'es2015', { modules: false } ],
+    [ 'es2017' ]
+  ],
+  plugins: ['transform-runtime', 'transform-decorators-legacy', 'transform-class-properties', 'transform-object-rest-spread']
+}
+
+const entryConfig = {
+  vendor: ['jquery'],
+  index: [
+    path.resolve(__dirname, 'app/index.js'),
+    path.resolve(__dirname, 'app/sass/main.scss')
+  ]
+}
+
+const outputConfig = {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'bundle.[name].[chunkhash].js'
+}
+
+const jsRules = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: 'babel-loader',
+      options: babelOptions
+    },
+    {
+      loader: 'eslint-loader'
+    }
+  ]
+}
+
+const tsRules = {
+  test: /\.ts(x?)$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: 'babel-loader',
+      options: babelOptions
+    },
+    {
+      loader: 'ts-loader'
+    },
+    {
+      loader: 'tslint-loader',
+      options: {
+        'tsConfigFile': 'tsconfig.json'
+      }
+    }
+  ]
+}
+
+const fontRules = {
+  test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+  use: [
+    { 
+      loader: 'file-loader',
+      options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        } 
+    }
+  ]
+}
+
+
+
+const sassRules = {
+  test: /\.scss$/,
+  exclude: /node_modules/,
+  use: extractPlugin.extract({
+    use: [
+      { 
+        loader: "css-loader",
+        options: {
+          sourceMap: true
+        } 
+      }, 
+      { 
+        loader: "postcss-loader",
+        options: {
+          sourceMap: 'inline'
+        } 
+      }, 
+      { 
+        loader: "sass-loader", 
+        options: {
+          sourceMap: true
+        }
+      }
+    ]
+  })
+}
+
+const htmlRules = {
+  test: /\.html$/,
+  exclude: /node_modules/,
+  use: ['html-loader']
+}
+
+const pugRules = {
+  test: /\.pug$/,
+  exclude: /node_modules/,
+  use: [
+    { loader: 'html-loader' }, 
+    { loader: 'pug-html-loader',
+      options: {
+        name: '[name],[ext]'
+      }
+    }
+  ]
+}
+
+const imageRules = {
+  test: /\.(jpg|png|ico|svg)$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: 'file-loader',
+      options: {
+        name: '[name],[ext]',
+        outputPath: 'images/',
+      }
+    }
+  ]
+}
+
+module.exports = (env = {}) => {
+  // Variables set by npm scripts in package.json
+  const isProduction = env.production === true
+
+  const minifyPlugin = new webpack.LoaderOptionsPlugin({
+    minimize: (isProduction) ? true : false,
+    debug: (isProduction) ? false : true
+  })
+
+  return {
+    entry: entryConfig,
+    output: outputConfig,
+
+    devtool: (() => {
+      return (isProduction) ? 'hidden-source-map' : 'cheap-module-eval-source-map'
+    })(),
+
+    module: {
+      rules: [ tsRules, jsRules, sassRules, fontRules, htmlRules, pugRules, imageRules ]
+    },
+
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+
+    plugins: [
+      extractPlugin,
+      providerPlugin,
+      cleanWebPackPlugin,
+
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js'
+      }),
+
+      new HtmlWebpackPlugin({
+        favicon: 'app/favicon.png',
+        template: 'app/index.pug',
+        filename: 'index.html',
+        chunnk: ['index']
+      }),
+      new HtmlWebpackPlugin({
+        favicon: 'app/favicon.png',
+        template: 'app/service.pug',
+        filename: 'service.html',
+        chunnk: ['index']
+      }),
+
+      minifyPlugin
+    ],
+
+    devServer: {
+      host: 'localhost',
+      port: 7000,
+      open: true,
+      historyApiFallback: true,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      }
+    }
+
+  }
+};
