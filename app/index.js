515707fa947bd05d22b6075dec02aa0f8ef5a51f
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-16 13:58:10 -0700

    fixed header

--- app/index.js
+++ app/index.js
@@ -1,25 +1,11 @@
 import 'jquery';
 
-import { helpMe } from './helper';
-import { checkAsyncResult } from './use-async';
-
-// eslint-disable-next-line 
-import legachLib from 'imports-loader?window=>{}!exports-loader?legacyLib!./non-es6-lib';  
-
-const siteSetup = () => {
-  console.log("Set up ... in arrow function now")
-}
-
-const testjQuery = () => {
-  const thisBody = $('body');
-  thisBody.css({ 'color': '#fff' })
+const fixedPageHeader = () => {
+  const pageHeader = document.querySelector('.js-pageHeader');
+  document.body.style.paddingTop = pageHeader.offsetHeight + 'px';
 }
 
 $(function () {
-  helpMe();
-  siteSetup();
-  checkAsyncResult();
-  testjQuery();
-  legachLib();
+  fixedPageHeader();
+  
 });
-
