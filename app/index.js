63c0f2638875f5bbf42a548a129a345c67d74e62
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-18 17:37:03 -0700

    update

--- app/index.js
+++ app/index.js
@@ -1,19 +1,25 @@
 import 'jquery';
 
-const fixedPageHeader = () => {
-  const pageHeader = document.querySelector('.js-pageHeader');
-  document.body.style.paddingTop = pageHeader.offsetHeight + 'px';
-}
-
 const setupMobileMenu = () => {
-  const mobileMemu = $('.js-mobileMenu');
-  mobileMemu.click((e) => {
-    e.preventDefault();
-    mobileMemu.toggleClass('s-showMenu');
+  $("#menu-button").click(function(){
+    $("#menu-bg").toggleClass("big");
+    if ($("#menu-button").hasClass('active')){
+      $("#menu-button").css('background','aqua');
+      $(".bar").css('background','#fff');
+    }
+    else{
+      $("#menu-button").css('background','#fff');
+      $(".bar").css('background','aqua');
+    }
+    
+    $("#menu-button").toggleClass('active');
+    $(".nav").toggleClass('show');
+    $(".bar:nth-child(1)").toggleClass('one');
+    $(".bar:nth-child(2)").toggleClass('two');
+    $(".bar:nth-child(3)").toggleClass('three');
   });
 }
 
 $(function () {
-  fixedPageHeader();
   setupMobileMenu();
 });
